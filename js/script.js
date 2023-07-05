9146f198bfce771ad5754d9214f68b7718660663
Kamran Aghayev <aghayev.kamran@outlook.com>
2023-07-05 05:30:28 +0400

    beta

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+// JavaScript code
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu');
+    const menuItem = document.querySelectorAll('.menu_item');
+    const hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
@@ -10,8 +11,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+            hamburger.classList.remove('hamburger_active');
+            menu.classList.remove('menu_active');
+        });
+    });
+});
